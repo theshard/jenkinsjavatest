86d52b46ffee8cc467b6308a48240abca39651dd
JohnH <johnh@admins-MacBook-Pro-7.local>
2018-04-29 09:19:02 +0100

    changed to 15

--- src/main/java/junit1.java
+++ src/main/java/junit1.java
@@ -1,5 +1,8 @@
 public class junit1 {
     public static void main(String[] args){
         System.out.println("From the main of junit1");
+        for (int i=1; i<=15; i++){
+            System.out.println("value of i = "+ i);
+        }
     }
 }
